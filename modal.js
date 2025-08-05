cd01ff53de7270f41c68951be56d6cbaefb2da15
Jehimy <jehimyhernandez07@gmail.com>
2025-08-05 10:46:11 -0500

    Mantener solo formulario.html y estilos.css en front-html

--- modal.js
+++
@@ -1,9 +0,0 @@
-export function showModal() {
-    const modal = document.getElementById('modal-success');
-    modal.classList.remove('hidden');
-}
-
-export function hideModal(){
-    const modal = document.getElementById('modal-success')
-    modal.classList.add('hidden')
-}
